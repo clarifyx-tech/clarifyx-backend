16b1606827d535c202d031b897a04bcf12864ace
Krishna Madhavan <krishnamadhavan07@gmail.com>
2024-10-10 23:13:41 +0530

    conf: update simplejwt configurations

--- config/settings/local.py
+++ config/settings/local.py
@@ -8,11 +8,6 @@ from .base import env
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = True
-# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = env(
-    "DJANGO_SECRET_KEY",
-    default="kJ3PoaFjCxPg4FOdtNojLww2ydssmGgkkXMaRiClA75aNSUiN0qqUPALnOXJ6z2V",
-)
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
 
